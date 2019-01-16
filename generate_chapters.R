cca63bb1a8500d57c2e5c48b75fd52e9fdccf8b5
Kees Mulder <keestimmulder@gmail.com>
2019-01-16 22:32:19 +0100

    Initial file gather

--- generate_chapters.R
+++ generate_chapters.R
@@ -1,13 +1,12 @@
 library(stringr)
+library(tidyverse)
 
 out_folder <- "C:/Dropbox/Research/PhDDissertationBCS/chapters/"
 glm_path <- "C:/Dropbox/Research/BayesMultCircCovariates/Article JMP/"
 
 
 inside_curly <- Vectorize(function(string) {
-  string <- stringr::str_split(string, "\\{")[[1]][2]
-  string <- stringr::str_split(string, "\\}")[[1]][1]
-  string
+  stringr::str_extract(string, "\\{.*?\\}(?!.*\\})")
 })
 
 # Read a phd chapter and remove junk.
@@ -45,7 +44,11 @@ read_and_refactor_chapter <- function(input_path) {
 
 
 read_and_save_multiple_chapters <- function(filepaths,
-                                            outloc = "C:/Dropbox/Research/PhDDissertationBCS/chapters/") {
+                                            outloc = "C:/Dropbox/Research/PhDDissertationBCS/",
+                                            remove_preamble_lines = character(0)) {
+
+  outloc_ch <- paste0(outloc, "chapters/")
+  outloc_ch
 
   # Standard naming.
   if (is.null(names(filepaths))) {
@@ -54,28 +57,75 @@ read_and_save_multiple_chapters <- function(filepaths,
     nms <- names(filepaths)
   }
 
+  preamble_lines <- character(0)
+
   # Refactor and save.
   for (filepath_nm in nms) {
     filepath <- filepaths[filepath_nm]
     chapter <- read_and_refactor_chapter(filepath)
 
-    cat("Read chapter '", chapter$title, "'. Processing...\n", sep = "")
+    cat("Read chapter '", chapter$title, "'. Processing...\n---\n", sep = "")
 
-    saveRDS(chapter, file = paste0(outloc, filepath_nm, ".rds"))
+    preamble_lines <- c(preamble_lines,
+                        chapter$usepackage_lines,
+                        chapter$new_commands)
 
-    filecon <- file(paste0(outloc, filepath_nm, ".tex"))
+
+    saveRDS(chapter, file = paste0(outloc_ch, filepath_nm, ".rds"))
+
+    filecon <- file(paste0(outloc_ch, filepath_nm, ".tex"))
     writeLines(chapter$text, con = filecon)
     close(filecon)
+
   }
+
+
+  # COMMAND LINES
+  cmd_lines_sep <- sapply(preamble_lines, function(x) {
+    locs <- str_locate_all(x, pattern = "\\\\newcommand")[[1]]
+    str_sub(x, start = locs[, 1], end =  c(locs[-1, 1] - 1, nchar(x)))
+  })
+  pkg_lines_sep <- sapply(preamble_lines, function(x) {
+    locs <- str_locate_all(x, pattern = "\\\\usepackage")[[1]]
+    str_sub(x, start = locs[, 1], end =  c(locs[-1, 1] - 1, nchar(x)))
+  })
+
+  pream_lines_sep <- c(pkg_lines_sep, cmd_lines_sep)
+
+  pream_lines_sep %>%
+    unlist %>%
+    str_trim %>%
+    unique %>%
+    sort() -> plu
+
+  # Manual fixes.
+  plu <- plu[!(plu %in% remove_preamble_lines)]
+
+  # Remove Knitr commands which would be double.
+  plu <- plu[!grepl("^(\\\\newcommand\\{\\\\hl)", plu)]
+
+  cat("Writing usepackage and newcommand lines.\n")
+  filecon <- file(paste0(outloc, "usepkg_newcommand.tex"))
+  writeLines(plu, con = filecon)
+  close(filecon)
 }
+
+
+manual_fixes <- c("\\newcommand{\\sumin}{\\sum_{i = 1}^n}",
+                  "\\newcommand{\\bX}{\\boldsymbol{\\Theta}}",
+                  "\\newcommand{\\bx}{\\boldsymbol{\\theta}}",
+                  "\\newcommand{\\thedata}{\\bt, \\bX, \\bd}",
+                  "\\newcommand{\\wavg}{\\frac{1}{n} \\sum_{i=1}^n}",
+                  "\\usepackage{upquote}}{}")
+
 
 resfol <- "C:/Dropbox/Research/"
 
-chapter_texs <- c(circ_glm = paste0(resfol, "BayesMultCircCovariates/Article JMP/EstAndHypTestBayesCircGLM.tex"),
-                  hypotest = paste0(resfol, "BayesIsop/Spread/JSPI/BayesIsop.tex"),
-                  flexcmix = paste0(resfol, "IVDRSaccades/Spread/Article/JMP/FlexibleModelingOfSaccadeDirectionDistributions.tex"),
-                  revrjump = paste0(resfol, "VonMisesMixtureReversibleJump/JSCS/VonMisesMix_RevJump_submission.tex"),
-                  dpm_crim = paste0(resfol, "AoristicAnalysis/Spread/Article/DealingWithPartiallyObservedCrimeTimes.tex"),
-                  circbays = paste0(resfol, "circbayes_paper/circbayes_RPackageForBayesianCircularStatistics/circbayes_RPackageForBayesianCircularStatistics.tex"))
+filepaths <- c(circ_glm = paste0(resfol, "BayesMultCircCovariates/Article JMP/EstAndHypTestBayesCircGLM.tex"),
+               hypotest = paste0(resfol, "BayesIsop/Spread/JSPI/BayesIsop.tex"),
+               flexcmix = paste0(resfol, "IVDRSaccades/Spread/Article/JMP/FlexibleModelingOfSaccadeDirectionDistributions.tex"),
+               revrjump = paste0(resfol, "VonMisesMixtureReversibleJump/JSCS/VonMisesMix_RevJump_submission.tex"),
+               dpm_crim = paste0(resfol, "AoristicAnalysis/Spread/Article/DealingWithPartiallyObservedCrimeTimes.tex"),
+               circbays = paste0(resfol, "circbayes_paper/circbayes_RPackageForBayesianCircularStatistics/circbayes_RPackageForBayesianCircularStatistics.tex"))
 
-read_and_save_multiple_chapters(chapter_texs)
+read_and_save_multiple_chapters(filepaths, remove_preamble_lines = manual_fixes)
